1aecd8b44d9567679a30e7f342c256d7bcc342cd
kemosaaabe <sanchellos27@mail.ru>
2023-04-10 09:27:14 +0300

    [+] Ticker component

--- client/src/components/Ticker/index.tsx
+++ client/src/components/Ticker/index.tsx
@@ -1,27 +1,43 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC } from 'react';
 import styles from './Ticker.module.scss';
 
 const tickerData = [];
 
 const Ticker: FC = () => {
+    const [activeValue, setActiveValue] = React.useState('');
+
+    const activeValueOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const intValue = e.target.value.replace(/\D/g, '');
+        setActiveValue(intValue);
+    };
+
     return (
         <div className={styles.ticker}>
-            <div className={styles.selectFields}>
-                <select name="tradeActive">
-                    <option value="">USD</option>
-                    <option value="">EUR</option>
-                    <option value="">RUB</option>
-                </select>
-                <input type="text" name="volume" />
-            </div>
-            <div className={styles.btnsBlock}>
-                <div className={styles.sellBlock}>
-                    <span></span>
-                    <button className={styles.btnSell}>Sell</button>
+            <h2>Ticker</h2>
+            <div className={styles.fieldsWrapper}>
+                <div className={styles.selectFields}>
+                    <select name="tradeActive">
+                        <option value="">USD</option>
+                        <option value="">EUR</option>
+                        <option value="">RUB</option>
+                    </select>
+                    <input
+                        placeholder="Active value..."
+                        type="text"
+                        name="volume"
+                        value={activeValue}
+                        onChange={activeValueOnChange}
+                    />
                 </div>
-                <div className={styles.buyBlock}>
-                    <span></span>
-                    <button className={styles.btnBuy}>Buy</button>
+                <div className={styles.btnsBlock}>
+                    <div className={styles.btnWrapper}>
+                        <span>8.559</span>
+                        <button className={styles.btnSell}>Sell</button>
+                    </div>
+                    <div className={styles.btnWrapper}>
+                        <span>8.557</span>
+                        <button className={styles.btnBuy}>Buy</button>
+                    </div>
                 </div>
             </div>
         </div>
